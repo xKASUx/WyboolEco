0a8439c5bf99d7aef66f64721dbb02155920c462
Kirill <117340458+yalisoff@users.noreply.github.com>
2024-09-19 16:48:12 +0700

    usrefactor auth / reg

--- src/core/models/SupportModel.php
+++ src/core/models/SupportModel.php
@@ -31,28 +31,29 @@ class SupportModel
         );
     }
 
-    public function handlerReg($name, $surname, $patronymic, $email, $phone, $password, $role) {
+    public function handlerReg($name, $surname, $patronymic, $email, $phone, $password, $role) // Регистрация пользователя
+    {
         $query = $this->db->query("INSERT INTO `users` (`user_role`, `user_name`, `user_surname`, `user_patronymic`, `user_phone`, `user_email`, `user_password`) VALUES (:role, :name, :surname, :patronymic, :phone, :email, :password)", [
-            ':role'=>$role,
-            ':name'=>$name,
-            ':surname'=>$surname,
-            ':patronymic'=>$patronymic,
-            ':phone'=>$phone,
-            ':email'=>$email,
-            ':password'=>$password,
+            ':role' => $role,
+            ':name' => $name,
+            ':surname' => $surname,
+            ':patronymic' => $patronymic,
+            ':phone' => $phone,
+            ':email' => $email,
+            ':password' => $password,
         ]);
-
+    
         $user_id = $this->db->lastInsertId();
-
+    
         return [
-            'user_id'=>$user_id,
-            'user_role'=>$role,
-            'user_name'=>$name,
-            'user_surname'=>$surname,
-            'user_patronymic'=>$patronymic,
-            'user_points'=>0,
+            'user_id' => $user_id,
+            'user_role' => $role,
+            'user_name' => $name,
+            'user_surname' => $surname,
+            'user_patronymic' => $patronymic,
+            'user_points' => 0,
         ];
-    }
+    }      
 
     public function handlerUlConfirm()
     {
--- src/core/services/SupportService.php
+++ src/core/services/SupportService.php
@@ -15,90 +15,94 @@ class SupportService
         $this->supportModel = new SupportModel(); // Создания экземпляра модели "Дополнительно"
     }
 
-    public function handlerAuth($data) {
+    public function handlerAuth($data) 
+    {
         $email = $data['email'];
         $password = $data['password'];
-
+    
         $dataCheckEmail = $this->supportModel->checkEmail($email);
-
+    
         $dataCount = $dataCheckEmail[0]['COUNT(*)'];
-
+    
         if ($dataCount == 0) {
             echo "Аккаунт с данной почтой не найден";
         } else {
-            $data = $this->supportModel->checkSign($email);
-            if ($data[0]['user_password'] === $password) {
-                $_SESSION['user']['id'] = $data[0]['user_id'];
-                $_SESSION['user']['role'] = $data[0]['user_role'];
-                $_SESSION['user']['name'] = $data[0]['user_name'];
-                $_SESSION['user']['surname'] = $data[0]['user_surname'];
-                $_SESSION['user']['patronymic'] = $data[0]['user_patronymic'];
-                $_SESSION['user']['phone'] = $data[0]['user_phone'];
-                $_SESSION['user']['email'] = $data[0]['user_email'];
-                $_SESSION['user']['password'] = $data[0]['user_password'];
-                $_SESSION['user']['ul_confirm'] = $data[0]['user_ul_confirm'];
-                $_SESSION['user']['points'] = $data[0]['user_points'];
-                if (empty($_SESSION['user'])) {
-                    header('Location: /auth');
-                } else {
-                    header('Location: /profile');
-                }
+            $userData = $this->supportModel->checkSign($email);
+            
+            if (password_verify($password, $userData[0]['user_password'])) {
+                $_SESSION['user'] = [
+                    'id' => $userData[0]['user_id'],
+                    'role' => $userData[0]['user_role'],
+                    'name' => $userData[0]['user_name'],
+                    'surname' => $userData[0]['user_surname'],
+                    'patronymic' => $userData[0]['user_patronymic'],
+                    'phone' => $userData[0]['user_phone'],
+                    'email' => $userData[0]['user_email'],
+                    'ul_confirm' => $userData[0]['user_ul_confirm'],
+                    'points' => $userData[0]['user_points']
+                ];
+    
+                header('Location: /profile');
             } else {
                 echo 'Пароль неверный';
             }
         }
     }
 
-    public function handlerReg($data) {
-        $name = $data['name'];
-        $surname = $data['surname'];
-        $patronymic = $data['patronymic'];
-        $email = $data['email'];
-        $phone = $data['phone'];
-        $password = $data['password'];
-        $confirm_password = $data['confirm_password'];
-        $role = $data['role'];
-
-        $dataCheckEmail = $this->supportModel->checkEmail($email);
-
-        $dataCount = $dataCheckEmail[0]['COUNT(*)'];
-
-        if ($dataCount != 0) {
-            echo "Аккаунт с данной почтой уже существует";
-        } else {
-            if ($password === $confirm_password) {
-                if ($patronymic == '') {
-                    $patronymic = NULL;
-                }
-
-                if ($phone == '+7 (___) ___-__-__') {
-                    $phone = NULL;
-                }
-
-                $this->supportModel->handlerReg($name, $surname, $patronymic, $email, $phone, $password, $role);
-
-                $data = $this->supportModel->checkSign($email);
-
-                $_SESSION['user']['id'] = $data[0]['user_id'];
-                $_SESSION['user']['role'] = $data[0]['user_role'];
-                $_SESSION['user']['name'] = $data[0]['user_name'];
-                $_SESSION['user']['surname'] = $data[0]['user_surname'];
-                $_SESSION['user']['patronymic'] = $data[0]['user_patronymic'];
-                $_SESSION['user']['phone'] = $data[0]['user_phone'];
-                $_SESSION['user']['email'] = $data[0]['user_email'];
-                $_SESSION['user']['password'] = $data[0]['user_password'];
-                $_SESSION['user']['ul_confirm'] = $data[0]['user_ul_confirm'];
-                $_SESSION['user']['points'] = $data[0]['user_points'];
-                if (empty($_SESSION['user'])) {
-                    header('Location: /auth');
-                } else {
-                    header('Location: /profile');
-                }
+        public function handlerReg($data) 
+        {
+            $name = $data['name'];
+            $surname = $data['surname'];
+            $patronymic = $data['patronymic'];
+            $email = $data['email'];
+            $phone = $data['phone'];
+            $password = $data['password'];
+            $confirm_password = $data['confirm_password'];
+            $role = $data['role'];
+        
+            $dataCheckEmail = $this->supportModel->checkEmail($email);
+            $dataCount = $dataCheckEmail[0]['COUNT(*)'];
+        
+            if ($dataCount != 0) {
+                echo "Аккаунт с данной почтой уже существует";
             } else {
-                echo 'Пароли не совпадают';
+                if ($password === $confirm_password) {
+                    if ($patronymic == '') {
+                        $patronymic = NULL;
+                    }
+        
+                    if ($phone == '+7 (___) ___-__-__') {
+                        $phone = NULL;
+                    }
+        
+                    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        
+                    $this->supportModel->handlerReg($name, $surname, $patronymic, $email, $phone, $hashedPassword, $role);
+        
+                    $data = $this->supportModel->checkSign($email);
+        
+                    $_SESSION['user']['id'] = $data[0]['user_id'];
+                    $_SESSION['user']['role'] = $data[0]['user_role'];
+                    $_SESSION['user']['name'] = $data[0]['user_name'];
+                    $_SESSION['user']['surname'] = $data[0]['user_surname'];
+                    $_SESSION['user']['patronymic'] = $data[0]['user_patronymic'];
+                    $_SESSION['user']['phone'] = $data[0]['user_phone'];
+                    $_SESSION['user']['email'] = $data[0]['user_email'];
+                    $_SESSION['user']['ul_confirm'] = $data[0]['user_ul_confirm'];
+                    $_SESSION['user']['points'] = $data[0]['user_points'];
+        
+                    unset($_SESSION['user']['password']);
+        
+                    if (empty($_SESSION['user'])) {
+                        header('Location: /auth');
+                    } else {
+                        header('Location: /profile');
+                    }
+                } else {
+                    echo 'Пароли не совпадают';
+                }
             }
-        }
-    }
+        }        
 
     public function handlerLogout() {
         session_unset();
